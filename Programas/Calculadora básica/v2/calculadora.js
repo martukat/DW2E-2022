8817bd7c9c8d3027550f652be9b5e3e26bf5f124
Santiago Aréchaga <santiago@no-spam.com>
2022-12-01 16:41:28 +0100

    Con función constructor

--- Programas/Calculadora básica/v2/calculadora.js
+++ Programas/Calculadora básica/v2/calculadora.js
@@ -6,24 +6,31 @@ var lista_operadores_texto = [];
 var lista_operadores = [];
 var inicializado = false;
 
+function objeto_operador(funcion, texto, ayuda, operandos) {
+    this.funcion = funcion;
+    this.texto = texto;
+    this.ayuda = ayuda;
+    this.operandos = (operandos === undefined) ? 2 : operandos;
+};
+
 function inicializar_operadores() {
     let operador = new Object();
     operador.funcion = (op1, op2) => op1 + op2;
     operador.texto = "+";
-    operador.ayuda = "Suma de los dos números";
-
     lista_operadores.push(operador);
 
-    operador = new Object();
-    operador.funcion = (op1, op2) => op1 - op2;
-    operador.texto = "-";
-    operador.ayuda = "Diferencia de los dos números";
+    operador = new objeto_operador(
+        (op1, op2) => op1 - op2,
+        "-", "Diferencia de los dos números");
+    lista_operadores.push(operador);
 
-    operador = new Object();
-    operador.funcion = (op1) => -op1;
-    operador.texto = "neg";
-    operador.ayuda = "Cambio de signo";
-    operador.operandos = 1;
+    operador = {
+        funcion: (op1) => -op1,
+        texto: "neg",
+        ayuda: "Cambio de signo",
+        operandos: 1,
+    };
+
 
     lista_operadores.push(operador);
 
@@ -56,8 +63,24 @@ function on_combo_change_mostrar_ayuda() {
     let operador = lista_operadores[indice];
 
     let ayuda = window.document.getElementById("ayuda");
-    ayuda.textContent = operador.ayuda;
+    let texto_ayuda = operador.ayuda;
+    ayuda.textContent = (texto_ayuda === undefined) ? "No hay ayuda disponible" : texto_ayuda;
 } // on_combo_change_mostrar_ayuda
+
+function on_combo_change_habilitar_operandos() {
+    let combo = (window.document.getElementById("combo_operador"));
+    let indice = Number(combo.value);
+    let operador = lista_operadores[indice];
+
+    let operandos = operador.operandos;
+    if (operandos === undefined) operandos = 2;
+
+    let input1 = window.document.getElementById("input_operando_1");
+    input1.disabled = (operandos < 1);
+
+    let input2 = window.document.getElementById("input_operando_2");
+    input2.disabled = (operandos < 2);
+} // on_combo_change_habilitar_operandos
 
 function inicializar_calculadora() {
     if (inicializado) return;
@@ -74,9 +97,11 @@ function inicializar_calculadora() {
         combo.append(option);
     } // for i
 
-    on_combo_change_mostrar_ayuda();
     combo.addEventListener("change", on_combo_change_mostrar_ayuda);
-    // combo.addEventListener("change", on_combo_change_habilitar_operandos);
+    combo.addEventListener("change", on_combo_change_habilitar_operandos);
+
+    on_combo_change_mostrar_ayuda();
+    on_combo_change_habilitar_operandos();
 
     inicializado = true;
 } // inicializar_calculadora
